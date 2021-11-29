7de68b2e47e103a801c847834a07497b889660f6
Hibikeyuri <hibikeyuri@gmail.com>
2021-11-29 18:42:33 +0800

    update

--- UVa/UVa_11417.cpp
+++ UVa/UVa_11417.cpp
@@ -1,39 +1,6 @@
 #include <stdio.h>
 
-double pow(double x, int n){
-    double answer = 1;
-    return answer;
-}
-
-double optimizedPow(double x, int n){
-    return 0;
-}
-
-double odst(){
-    return 0;
-}
-
-double oni(){
-    return 0;
-    return 1;
-    return 0;
-    return 1;
-    return 0;
-    return 1;
-}
-
 int main()
 {
-    double x;
-    int n;
-    if(n == 0){
-        return 1;
-    }
-    if(n % 2 == 0){
-        return optimizedPow(x*x, n/2);
-    }
-    else{
-        return x * optimizedPow(x*x, n/2);
-    }
     return 0;
 }
