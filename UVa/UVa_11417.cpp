6a0e4b17f136f0874d0d10f97faca0e39fef1170
Hibikeyuri <hibikeyuri@gmail.com>
2021-12-12 21:44:48 +0800

    update

--- UVa/UVa_11417.cpp
+++ UVa/UVa_11417.cpp
@@ -2,7 +2,18 @@
 
 int main()
 {
-    return 0;
-    return 1;
+    int a[100], b[100];
+    int n = 0;
+    a[0] = 0;
+    for(int j=0; i<n; ++j){
+        q = -100;
+        for(int i=0; i<j; ++i){
+            if(q < b[i] + a[j-i]){
+                q = b[i] + a[j-i];
+            }
+        }
+        a[j] = q;
+    }
+    return b;
 }
 
