c56b00e33a3683cd7f1ba13766debf94a94f850f
Hibikeyuri <hibikeyuri@gmail.com>
2021-12-14 23:11:54 +0800

    waste time

--- UVa/UVa_11417.cpp
+++ UVa/UVa_11417.cpp
@@ -2,20 +2,6 @@
 
 int main()
 {
-    int a[100];
-    int b[100];
-    int n = 0;
-    int q = 0;
-    a[0] = 0;
-    for(int i=0; i<n; ++j){
-        q = -100;
-        for(int i=0; i<j; ++i){
-            if(q < b[i] + a[j-i]){
-                q = b[i] + a[j-i];
-            }
-        }
-        a[j] = q;
-    }
-    return b[0];
+    return;
 }
 
